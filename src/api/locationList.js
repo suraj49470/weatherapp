b5f01006ad12bea9f3e832c59bd2805804252970
Suraj Singh <suraj49470@gmail.com>
2021-10-06 10:46:13 +0530

    updated read me and added not for api endpoint

--- src/api/locationList.js
+++ src/api/locationList.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-
+/*
+        NOTE :  https://www.metaweather.com/api/ is giving CORS issue when accessed via application.
+                so i have used  https://meta-weather.vercel.app/api endpoint which internally calls https://www.metaweather.com/api/ 
+                endpoint and resolves CORS issue.
+                
+*/
 const baseUrl = 'https://meta-weather.vercel.app/api/location/search/?';
 
 const getBaseUrl = (type) => {
